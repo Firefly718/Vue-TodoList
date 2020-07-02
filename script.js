77681ab5acb114f113029d9085a0a0f98d52183a
Firefly718 <System.BLA.16@yandex.ru>
2020-07-02 21:12:21 +0300

    added delete-button and edited second page

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ Vue.component('task', {
             <button class="task__done" @click="task_done()">
                 <i class="fas fa-check-circle"></i>
             </button>
+
+            <button class="task__remove" @click="task_remove()">
+                <i class="fas fa-trash-alt"></i>
+            </button>
         </div>
     </div>
     `
@@ -41,10 +45,44 @@ var vue = new Vue({
                 title: 'Explore questions',
                 desc: 'Explore all confusing moments about Vue.JS from the MindMap.'
             }
+        ],
+        tasks_second: [
+            {
+                title: 'Go to sleep',
+                desc: 'First - you should to have a good sleep.'
+            },
+            {
+                title: 'Actions after awakening',
+                desc: 'After your awakening (aprox. 6-8h of sleep) you need doing smt from 1 minute to 50 minutes. For example, drink a couple of water, go to the bathroom, make a meditation and etc. '
+            },
+            {
+                title: 'Next sleep',
+                desc: 'After your activity you need take a next sleep and wait a next, important awakening for getting the phase.'
+            },
+            {
+                title: 'Enter to phase',
+                desc: 'When you will feel your next awakening, you have to try enter to phase (preferably with closed eyes).'
+            },
+            {
+                title: 'Enter to phase - actions',
+                desc: 'You need to catch the moment and try to fly out of the body (without your muscle power).'
+            },
+            {
+                title: 'Entering',
+                desc: 'If you will fly out of the body you need to do smt quickly (for example, you can wave your hands and grope smt in dark, trying to see smt).'
+            },
+            {
+                title: 'You are in phase',
+                desc: 'Congratulations! If you can see smt, you can everything in the phase. Good luck!'
+            },
+            {
+                title: 'Wake up Neo...',
+                desc: 'The Matrix has you.'
+            }
         ]
     },
     methods: {
-        delete_task(id) {
+        done_task(id) {
             this.tasks.splice(id, 1);
         },
         add_task() {
